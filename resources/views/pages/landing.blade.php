f6bb8108fc978124640c5342e4406502ac07dacd
HardyValen <hardyvalen@gmail.com>
2020-01-26 19:33:08 +0700

    Scaffolding

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,10 @@
+@extends('template.main')
+
+@section('title')
+    9th Grand Summit &ndash; Student&#x2715;CEOs
+@endsection
+
+@section('content')
+    
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function() {return redirect()->to("/landing");});
+Route::get('/landing', 'PageRequestController@viewLanding');
