474e52f1bbc9b214e25a3768fa159777d60e27b9
BaconLover307 <kresnadi30@gmail.com>
2020-01-28 19:06:07 +0700

    Lumayan lah

---
+++ resources/views/pages/businesscase.blade.php
@@ -0,0 +1,29 @@
+@extends('template.main')
+
+@section('title')
+    9th Grand Summit &ndash; Student&#x2715;CEOs
+@endsection
+
+@section('content')
+    <main>
+        <section class="bc-hero">
+            <h1>Business Case Competition</h1>
+        </section>
+
+        <section class="bc-brief">
+            <h1>A Brief Description:</h1>
+            <p>Business Case Competition is a business based problem-solving competition.
+                Through this program, teams must identify the most prominent issues faced
+                by the company and develop a strategy to provide a solution to help
+                companies achieve their goals. They are expected to solve the problem in
+                forms of paper. Winning strategies include being detail-oriented, creative,
+                and business acumen when presenting recommendations.
+            </p>
+        </section>
+        <section class="bc-flow"></section>
+        <section class="bc-schedule"></section>
+        <section class="bc-jury"></section>
+        <section class="bc-regis"></section>
+        <section class="bc-sponsor"></section>
+    </main>
+@endsection
