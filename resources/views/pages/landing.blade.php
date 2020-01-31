d8efef88b267e3fa8171443f999f24c40c117dd9
BaconLover307 <kresnadi30@gmail.com>
2020-01-31 10:21:09 +0700

    Masih blom bisa tampil

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -5,7 +5,30 @@
 @endsection
 
 @section('content')
+<main>
+        <section class="bc-hero">
+            <h1>Business Case Competition</h1>
+        </section>
+
+        <section class="bc-brief">
+            <h1>A Brief Description:</h1>
+            <p>Business Case Competition is a business based problem-solving competition.
+                Through this program, teams must identify the most prominent issues faced
+                by the company and develop a strategy to provide a solution to help
+                companies achieve their goals. They are expected to solve the problem in
+                forms of paper. Winning strategies include being detail-oriented, creative,
+                and business acumen when presenting recommendations.
+            </p>
+        </section>
+        <section class="bc-flow"></section>
+        <section class="bc-schedule"></section>
+        <section class="bc-jury"></section>
+        <section class="bc-regis"></section>
+        <section class="bc-sponsor"></section>
+    </main>
+<!--     
     <main>
+    
         <!DOCTYPE html>
 <html>
 
@@ -22,5 +45,7 @@
     <h1 style="font-family:'Myriad Pro Bold Condensed Italic';font-weight:normal;font-size:42px">AaBbCcDdEeFfGgHhŞşIıİi Example</h1>
 
     </main>
+
+-->
 @endsection
 
