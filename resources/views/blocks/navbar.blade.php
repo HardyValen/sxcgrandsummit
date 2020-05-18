e34326bfcd58076faa0ffb3c2c5a8cd76197d7bb
BaconLover307 <kresnadi30@gmail.com>
2020-05-18 21:39:59 +0700

    Working on navbar

--- resources/views/blocks/navbar.blade.php
+++ resources/views/blocks/navbar.blade.php
@@ -4,12 +4,22 @@
     </div>
     <div class="navbar-links">
         <a href="/landing">HOME</a>
-        {{-- <a href="/about">ABOUT</a> --}}
-        <a href="/landing#event" id="navEvent">EVENT</a>
+        {{-- <a href="/landing#event" id="navEvent">EVENT</a> --}}
+        <div class="dropdown">
+            <button class="dropbtn">EVENT
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="/business-case">Business Case Competition</a>
+                <a href="/pre-event">Pre Event</a>
+                <a href="/summit">The Summit</a>
+            </div>
+        </div>
+
         <a href="/landing#schedule" id="navSchedule">SCHEDULE</a>
     </div>
     <div class="navbar-registration">
-        <a href="/landing#registration" class="button-primary" id="navRegist">REGISTRATION</a>
+        <a href="/landing#registration" class="button-primary-transparent" id="navRegist">Registration</a>
     </div>
     <div class="navbar-mobile-nav">
         <a onclick="showsidebar()">
