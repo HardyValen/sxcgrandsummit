d0c33fe3d0b06956a0fa5bdd86765531837220d7
Mona Lisa <13518147@std.stei.itb.ac.id>
2020-06-09 19:13:02 +0700

    Add preevent3 form and revise model

--- app/Preevent3Member.php
+++ app/Preevent3Member.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Preevent3Member extends Model
 {
-    //
+    public $timestamps = false;
+    public $emailException;
+    
+    protected $table = 'preevent3_members';
+
+    protected $fillable = [
+        "fullname",
+        "gender",
+        "hometown",
+        "email",
+        "phone",
+        "major",
+        "university",
+    ];
+
+    protected $primaryKey = 'preevent3_member_id';
 }
